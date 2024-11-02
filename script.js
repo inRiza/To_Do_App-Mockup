fd60a8c393471e7ad26869821295cc548a26d452
MRizainF <icon.firdaus@gmail.com>
2024-11-02 11:38:13 +0700

    add script for active navbar

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-
+/* for sidebar pops out */
 function sidebarClick() {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.display = sidebar.style.display === "flex" ? "none" : "flex";
 }
+
+function navClick(click) {
+    const navLink = document.querySelectorAll(".nav_link");
+    
+    navLink.forEach(link => link.classList.remove("active"));
+
+    click.currentTarget.classList.add("active");
+}
+
+document.querySelectorAll(".nav_link").forEach(link => {
+    link.addEventListener("click", navClick);
+});
