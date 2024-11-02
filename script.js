42bbb1c00a7068a78b423f442936459e9f7ae3ca
MRizainF <icon.firdaus@gmail.com>
2024-11-02 12:07:57 +0700

    add script for responsive sidebar

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function sidebarClick() {
     sidebar.style.display = sidebar.style.display === "flex" ? "none" : "flex";
 }
 
+/* for resizing if width > 860px */
+window.addEventListener("resize", () => {
+    const sidebar = document.getElementById("sidebar");
+    if (window.innerWidth > 860) {
+        sidebar.style.display = "none";
+    }
+});
+
+/* for clicking the navbar */
 function navClick(click) {
     const navLink = document.querySelectorAll(".nav_link");
-    
+
     navLink.forEach(link => link.classList.remove("active"));
 
     click.currentTarget.classList.add("active");
