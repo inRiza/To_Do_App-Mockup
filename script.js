43993326ac90bdae9867f5d5473b725275173e48
MRizainF <icon.firdaus@gmail.com>
2024-11-03 11:47:08 +0700

    feat: add script for task list etc

--- script.js
+++ script.js
@@ -32,3 +32,59 @@ function navigateTo(sectionId) {
 
     document.getElementById(sectionId).classList.add("active");
 }
+
+
+function addTask() {
+    const taskInput = document.getElementById("inputTask");
+    const taskList = document.getElementById("taskList");
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task_child");
+
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("check");
+        
+        const taskSpan = document.createElement("span");
+        taskSpan.textContent = taskText;
+
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("remove_button");
+        removeButton.onclick = function () {
+            removeTask(taskDiv);
+        }
+        taskDiv.appendChild(taskSpan);
+        taskDiv.appendChild(checkbox);
+        taskDiv.appendChild(removeButton);
+
+        taskList.appendChild(taskDiv);
+        
+        //checked checkbox design
+        document.querySelectorAll('.task_child input[type="checkbox"]').forEach(checkbox => {
+            checkbox.addEventListener("change", function() {
+                const taskChild = this.closest(".task_child");
+                if (this.checked) {
+                    taskChild.style.setProperty('background-color', 'rgba(0, 0, 0, 0.2)', 'important');
+                } else {
+                    taskChild.style.setProperty('background-color', '#b291e1', 'important');
+                }
+            });
+        });
+
+        taskInput.value = "";
+    }
+}
+
+document.getElementById("addNewTask").addEventListener("click", addTask);
+
+/* removing task */
+function removeTask(taskDiv) {
+    taskDiv.remove();
+}
+
+
